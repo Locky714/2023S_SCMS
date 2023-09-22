ff5474e1e7264a92b014af566d21ad816919a4d4
Lachlan Wright <locky.wright@gmail.com>
2023-09-22 13:49:06 +1000

    Calibration capture script
    Adding a calibration capture script to collect the images required to run calibration toolbox
    Includes a test set of images (mechatronics lab cb11.10)

---
+++ vision/Calibration/Calibration_Capture.m
@@ -0,0 +1,26 @@
+cam_rgb = webcam("Intel(R) RealSense(TM) Depth Camera 435i RGB");
+
+% cam_depth = webcam("Intel(R) RealSense(TM) Depth Camera 435i Depth");
+cam_rgb.set("Resolution","1920x1080");
+
+num_images = 30;
+
+tiledlayout("flow");
+
+for i =1:num_images
+    nexttile;
+    % Capture image
+    I_rgb = snapshot(cam_rgb);
+    % Convert to greyscale
+    I_gs = rgb2gray(I_rgb);
+    imshow(I_gs);
+    title("Image: "+i);
+    % File name string
+    name = "calib_"+i;
+    clc;
+    disp("Image: "+i+" / "+num_images);
+    %output to calibration images file
+    imwrite(I_gs,name+".jpg");
+    % Wait for user to reposition
+    pause;
+end
