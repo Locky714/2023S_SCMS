78af76e6f711298c3d976a80f43e98e2c3a8ca04
Lachlan Wright <locky.wright@gmail.com>
2023-10-03 15:43:53 +1100

    Create FeatureExtraction.m
    Adding FeatureExtraction.m function, outputs[tform,scale,rot] = inputs(I_reference,I_image,output_bool)

---
+++ vision/FeatureExtraction.m
@@ -0,0 +1,39 @@
+function [tform,scale,rot] = FeatureExtraction(image, reference,output)
+reference = rgb2gray(reference);
+image = rgb2gray(image);
+
+points1 = detectSURFFeatures(reference);
+points2 = detectSURFFeatures(image);
+
+[features1, validPoints1] = extractFeatures(reference,points1);
+[features2, validPoints2] = extractFeatures(image,points2);
+
+indexPairs = matchFeatures(features1,features2);
+matchedPoints1 = validPoints1(indexPairs(:,1));
+matchedPoints2 = validPoints2(indexPairs(:,2));
+
+[tform, inlierDistorted, inlierOriginal] = estimateGeometricTransform(matchedPoints2,matchedPoints1,'similarity');
+Tinv = tform.invert.T;
+ss = Tinv(2,1);
+sc = Tinv(1,1);
+scale = sqrt(ss*ss+sc*sc);
+rot = atan2(ss,sc)*180/pi;
+if output
+    figure('Name',"OUTPUT IMAGES")
+    tiledlayout('flow');
+    nexttile;
+    showMatchedFeatures(reference,image,matchedPoints1,matchedPoints2,'montage');
+    title("Showing matched pairs");
+
+    nexttile;
+    showMatchedFeatures(reference,image,inlierOriginal,inlierDistorted);
+    title("Matching points (inliers only)");
+    legend('ptsOriginal','ptsDistorted');
+    outputView = imref2d(size(image));
+    recovered = imwarp(image,tform, 'OutputView',outputView);
+
+    nexttile;
+    imshowpair(reference,recovered,'montage');
+    title('kfc1 and 2 - 2nd rotated to match 1st perspective');
+end
+end
