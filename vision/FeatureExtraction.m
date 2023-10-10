fece0a565f273eabf6a3ecf10e4645a67eb34cb2
Lachlan Wright <locky.wright@gmail.com>
2023-10-10 14:53:01 +1100

    More test images
    Adding test images and checking run time of different feature extraction. (SIFT fastest = 0.4 seconds for 600x 600 image).

--- vision/FeatureExtraction.m
+++ vision/FeatureExtraction.m
@@ -1,35 +1,34 @@
-function [tform] = FeatureExtraction(image, reference,output,type)
+function [T, scale] = FeatureExtraction(image, reference,output,type)
 if nargin ==0
-image = imread("tag_rot.jpg");
+image = imread("tag_rot_60.jpg");
 reference = imread("tag.jpg");
 end
 if nargin<3
-    output= 1;
+    output= 1; 
 end
 if nargin < 4
-    type = "KAZE";
+    type = "BRISK";
 end
+% 
+% image_harris_corners = detectHarrisFeatures(image,"MinQuality",0.3);
+% reference_harris_corners = detectHarrisFeatures(reference,"MinQuality",0.3);
+% 
+% figure("Name","Harris")
+% tiledlayout( "flow")
+% nexttile;
+% imshow(image);
+% hold on;
+% plot(image_harris_corners);
+% hold off
+% title("Image")
+% 
+% nexttile
+% imshow(reference);
+% hold on
+% plot(reference_harris_corners);
+% hold off
+% title("harris")
 
-image_harris_corners = detectHarrisFeatures(image,"MinQuality",0.3);
-reference_harris_corners = detectHarrisFeatures(reference,"MinQuality",0.3);
-
-figure("Name","Harris")
-tiledlayout( "flow")
-nexttile;
-imshow(image);
-hold on;
-plot(image_harris_corners);
-hold off
-title("Image")
-
-nexttile
-imshow(reference);
-hold on
-plot(reference_harris_corners);
-hold off
-title("harris")
-
-% pause;
 switch type
     case "SIFT"
         reference_points = detectSIFTFeatures(reference);
@@ -43,32 +42,36 @@ switch type
     case "BRISK"
         reference_points = detectBRISKFeatures(reference);
         image_points = detectBRISKFeatures(image);
-    case "FREAK"
-        reference_points = detectFreakFeatures(reference);
-        image_points = detectFreakFeatures(image);
     case "KAZE"
         reference_points = detectKAZEFeatures(reference);
         image_points = detectKAZEFeatures(image);
     otherwise
-        reference_points = detectSIFTFeatures(reference);
-        image_points = detectSIFTFeatures(image);
+        disp("NO TYPE SET");
 end
 
-[reference_features, reference_validPoints] = extractFeatures(reference,reference_points,"Upright",false,"Method",type); % points1
-[image_features, image_validPoints] = extractFeatures(image,image_points,"Upright",false,"Method",type); %points2
+[reference_features, reference_validPoints] = extractFeatures(reference,reference_points,"Upright",false);
+[image_features, image_validPoints] = extractFeatures(image,image_points,"Upright",false); 
 
 indexPairs = matchFeatures(reference_features,image_features);
 reference_matchedPoints = reference_validPoints(indexPairs(:,1));
 image_matchedPoints = image_validPoints(indexPairs(:,2));
 [tform, inlierDistorted, inlierOriginal] = estimateGeometricTransform(image_matchedPoints,reference_matchedPoints,'similarity');
+
+% Displaying scale and rotation of transform.
 Tinv = tform.invert.T;
 ss = Tinv(2,1);
 sc = Tinv(1,1);
 scale = sqrt(ss*ss+sc*sc);
-rot = atan2(ss,sc)*180/pi;
 
-disp("Rot = "+rot)
-disp("Scale = " + scale)
+% Accessing for output from function.
+T = tform.T;
+
+
+% rot = atan2(ss,sc)*180/pi;
+% 
+% disp("Rot = "+rot)
+% disp("Scale = " + scale)
+
 if output
     figure('Name',"OUTPUT IMAGES")
     tiledlayout('flow');
