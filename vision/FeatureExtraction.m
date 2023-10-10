ce1fc8b0ff5608b10fea40066f078cb0f3bc744f
Lachlan Wright <locky.wright@gmail.com>
2023-10-10 14:05:33 +1100

    Work on feature extraction
    Adding ref image tags and attempting different feature techniques

--- vision/FeatureExtraction.m
+++ vision/FeatureExtraction.m
@@ -1,31 +1,79 @@
-function [tform,scale,rot] = FeatureExtraction(image, reference,output)
-if nargin==2
-    output= 0;
+function [tform] = FeatureExtraction(image, reference,output,type)
+if nargin ==0
+image = imread("tag_rot.jpg");
+reference = imread("tag.jpg");
 end
-reference = rgb2gray(reference);
-image = rgb2gray(image);
+if nargin<3
+    output= 1;
+end
+if nargin < 4
+    type = "KAZE";
+end
 
-points1 = detectSURFFeatures(reference);
-points2 = detectSURFFeatures(image);
+image_harris_corners = detectHarrisFeatures(image,"MinQuality",0.3);
+reference_harris_corners = detectHarrisFeatures(reference,"MinQuality",0.3);
 
-[features1, validPoints1] = extractFeatures(reference,points1);
-[features2, validPoints2] = extractFeatures(image,points2);
+figure("Name","Harris")
+tiledlayout( "flow")
+nexttile;
+imshow(image);
+hold on;
+plot(image_harris_corners);
+hold off
+title("Image")
 
-indexPairs = matchFeatures(features1,features2);
-matchedPoints1 = validPoints1(indexPairs(:,1));
-matchedPoints2 = validPoints2(indexPairs(:,2));
+nexttile
+imshow(reference);
+hold on
+plot(reference_harris_corners);
+hold off
+title("harris")
 
-[tform, inlierDistorted, inlierOriginal] = estimateGeometricTransform(matchedPoints2,matchedPoints1,'similarity');
+% pause;
+switch type
+    case "SIFT"
+        reference_points = detectSIFTFeatures(reference);
+        image_points = detectSIFTFeatures(image);
+    case "SURF"
+        reference_points = detectSURFFeatures(reference);
+        image_points = detectSURFFeatures(image);
+    case "ORB"
+        reference_points = detectORBFeatures(reference);
+        image_points = detectORBFeatures(image);
+    case "BRISK"
+        reference_points = detectBRISKFeatures(reference);
+        image_points = detectBRISKFeatures(image);
+    case "FREAK"
+        reference_points = detectFreakFeatures(reference);
+        image_points = detectFreakFeatures(image);
+    case "KAZE"
+        reference_points = detectKAZEFeatures(reference);
+        image_points = detectKAZEFeatures(image);
+    otherwise
+        reference_points = detectSIFTFeatures(reference);
+        image_points = detectSIFTFeatures(image);
+end
+
+[reference_features, reference_validPoints] = extractFeatures(reference,reference_points,"Upright",false,"Method",type); % points1
+[image_features, image_validPoints] = extractFeatures(image,image_points,"Upright",false,"Method",type); %points2
+
+indexPairs = matchFeatures(reference_features,image_features);
+reference_matchedPoints = reference_validPoints(indexPairs(:,1));
+image_matchedPoints = image_validPoints(indexPairs(:,2));
+[tform, inlierDistorted, inlierOriginal] = estimateGeometricTransform(image_matchedPoints,reference_matchedPoints,'similarity');
 Tinv = tform.invert.T;
 ss = Tinv(2,1);
 sc = Tinv(1,1);
 scale = sqrt(ss*ss+sc*sc);
 rot = atan2(ss,sc)*180/pi;
+
+disp("Rot = "+rot)
+disp("Scale = " + scale)
 if output
     figure('Name',"OUTPUT IMAGES")
     tiledlayout('flow');
     nexttile;
-    showMatchedFeatures(reference,image,matchedPoints1,matchedPoints2,'montage');
+    showMatchedFeatures(reference,image,reference_matchedPoints,image_matchedPoints,'montage');
     title("Showing matched pairs");
 
     nexttile;
