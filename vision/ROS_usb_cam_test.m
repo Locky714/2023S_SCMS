87677377fbd6043a8da1fea5a65c6dcb1d06188d
Lachlan Wright <locky.wright@gmail.com>
2023-10-13 15:07:17 +1100

    Update ROS_usb_cam_test.m
    Altering with functionality for eth0 interface, and adding depth channel subscriber/display.

--- vision/ROS_usb_cam_test.m
+++ vision/ROS_usb_cam_test.m
@@ -1,9 +1,18 @@
 clc
 close all
 clear
-%% Initialise core and subscribers
 try
-    rosinit("pi");
+    rosshutdown
+    disp("ROSCORE Shutdown")
+catch ME
+    disp("No roscore running")
+end
+%% Initialise core and subscribers
+
+setenv('ROS_MASTER_URI','http://192.168.2.1:11311'); % LAN Connection - "rosinit(pi)" works for wlan0 ??????????
+setenv('ROS_MASTER_IP_ADDRESS','192.168.2.1');
+try
+    rosinit;
 catch
     disp("ROSCORE unable to start, may already be running, attempting to continue.")
 end
@@ -11,16 +20,28 @@ end
 % display available topics
 % rostopic list;
 
-sub1 = rossubscriber("/usb_cam/image_raw");
+sub_image = rossubscriber('/camera/color/image_raw');
+sub_depth = rossubscriber("/camera/depth/image_rect_raw");
+pause(1);
 
 %% Extract image
 % if still, vid =0
 loop = true;
-vid =0;
+vid =1;
+figure("Name","Stream: RGBD");
+tiledlayout(1,2);
 while(loop)
-    extract = sub1.receive;
-    I  = readImage(extract);
+    nexttile(1)
+    extract_image = receive(sub_image,3);
+    I  = readImage(extract_image);
     imshow(I);
+    
+    nexttile(2)
+    extract_depth = receive(sub_depth,3);
+    I_depth  = readImage(extract_depth);
+    imshow(I_depth,jet,DisplayRange=[]);
+    colorbar;
+
     if vid ==0
         loop = false;
     end
